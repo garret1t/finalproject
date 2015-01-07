dcf0cb7fcb5456274df74aef3d01b3addb579432
garret1t <garrettt3t@yahoo.com>
2015-01-07 14:21:30 -0500

    Enemy ai start

--- Final Project/Final Project/Final Project/Player.cs
+++ Final Project/Final Project/Final Project/Player.cs
@@ -213,7 +213,7 @@ namespace Final_Project
                 }
             }
         }
-        public override void Draw(SpriteBatch spriteBatch)
+        public void Draw(SpriteBatch spriteBatch)
         {
             int leftMargin = 100;
             int topMargin = 200;
